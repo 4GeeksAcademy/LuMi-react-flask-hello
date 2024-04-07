bb790219d049904b04ee766f8eae7d12a8c0ff61
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-04-07 00:26:47 +0000

    :sparkles: finished (hopefully)

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -5,9 +5,19 @@ import { useNavigate } from 'react-router-dom'
 export const Private = () => {
 	const { store, actions } = useContext(Context);
 	const navigate = useNavigate()
+
 	useEffect(() => {
-		if (!store.token) navigate('/login')
-	})
+		if (!store.token) {
+			navigate('/login');
+		} else {
+			const getUserInfo = async () => {
+				actions.getAuth();
+			};
+			getUserInfo();
+		}
+	}, []);
+
+	console.log(store.userInfo)
 	return (
 		<div>
 			{store.token && <h1>{store.userInfo.email}</h1>}
